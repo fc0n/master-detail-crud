f8f664fdbffae912a1dcd5440b419fe035bf7ab2
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-02-04 18:50:09 -0300

    criando o módulo de categorias / adicionando a barra de navegação / implementando o template do CategoryList

---
+++ src/app/pages/categories/categories.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CategoriesRoutingModule } from './categories-routing.module';
+
+import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+
+@NgModule({
+  declarations: [
+    CategoryListComponent,
+    CategoryFormComponent
+  ],
+  imports: [
+    CommonModule,
+    CategoriesRoutingModule
+  ]
+})
+export class CategoriesModule { }
