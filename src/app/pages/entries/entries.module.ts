6cc05c6996ef544aaa2317def38301b4000546e8
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-02-07 20:31:14 -0300

    adicionando a lista de receitas e despesas

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntriesRoutingModule } from './entries-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [ EntryListComponent ],
   imports: [
     CommonModule,
     EntriesRoutingModule
