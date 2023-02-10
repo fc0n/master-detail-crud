f0c72e94e63437aedf881511f929d2f128fd737a
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-02-09 22:24:04 -0300

    adicionando o formulário de Receitas e Despesas

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { EntryListComponent } from './entry-list/entry-list.component';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 import { EntriesRoutingModule } from './entries-routing.module';
 
 @NgModule({
-  declarations: [ EntryListComponent ],
+  declarations: [ 
+    EntryListComponent,
+    EntryFormComponent
+   ],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EntriesModule { }
