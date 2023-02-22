aa65544a5f27f1af38fa385dbab76e54511714d6
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-02-21 22:19:03 -0300

    utilizando injector e refatorando o categoryService

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 
 import { Category } from './cateroy.model';
+
+import { BaseResourceService } from '../../../shared/services/base-resource.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
+export class CategoryService extends BaseResourceService<Category> {
 
-  private apiPath: string = "api/categories"
-
-  constructor() { }
-
+  constructor( protected injector: Injector) {
+    super("api/categories", injector);
+   }
 }
--- src/app/shared/services/base-resource.service.ts
+++ src/app/shared/services/base-resource.service.ts
@@ -1,3 +1,4 @@
+import { Injector } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 import { Observable, throwError } from "rxjs";
@@ -7,66 +8,67 @@ import { BaseResourceModel } from "../models/base-resource.model";
 
 export abstract class BaseResourceService<T extends BaseResourceModel> {
 
-  protected http: HttpClient;
+    protected http: HttpClient;
 
-  constructor( protected apiPath: string ) { }
+    constructor( protected apiPath: string, protected injector: Injector) {
+        this.http = injector.get(HttpClient);
+    }
 
-  getAll(): Observable<T[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToResources)
-    )
-  }
+    getAll(): Observable<T[]> {
+        return this.http.get(this.apiPath).pipe(
+        catchError(this.handleError),
+        map(this.jsonDataToResources)
+        )
+    }
 
-  getById(id: number): Observable<T> {
-    const url = `${this.apiPath}/${id}`;
+    getById(id: number): Observable<T> {
+        const url = `${this.apiPath}/${id}`;
 
-    return this.http.get(url).pipe(
-      catchError(this.handleError),     
-      map(this.jsonDataToResource)
-    )
-  }
+        return this.http.get(url).pipe(
+        catchError(this.handleError),     
+        map(this.jsonDataToResource)
+        )
+    }
 
-  create(resource: T): Observable<T> {
-    return this.http.post(this.apiPath, resource).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToResource)
-    )
-  }
+    create(resource: T): Observable<T> {
+        return this.http.post(this.apiPath, resource).pipe(
+        catchError(this.handleError),
+        map(this.jsonDataToResource)
+        )
+    }
 
-  update(resource: T): Observable<T> {
-    const url = `${this.apiPath}/${resource.id}`;
+    update(resource: T): Observable<T> {
+        const url = `${this.apiPath}/${resource.id}`;
 
-    return this.http.put(url, resource).pipe(
-      catchError(this.handleError),
-      map(() => resource)
-    )
-  }
+        return this.http.put(url, resource).pipe(
+        catchError(this.handleError),
+        map(() => resource)
+        )
+    }
 
-  delete(id: number): Observable<any> {
-    const url = `${this.apiPath}/${id}`;
+    delete(id: number): Observable<any> {
+        const url = `${this.apiPath}/${id}`;
 
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(() => null)
-    )
-  }
+        return this.http.delete(url).pipe(
+        catchError(this.handleError),
+        map(() => null)
+        )
+    }
 
-  // PROTECTED METHODS
+    // PROTECTED METHODS
 
-  protected jsonDataToResources(jsonData: any[]): T[]{
-    const resources: T[] = [];
-    jsonData.forEach(element => resources.push(element as T));
-    return resources;
-  }
+    protected jsonDataToResources(jsonData: any[]): T[]{
+        const resources: T[] = [];
+        jsonData.forEach(element => resources.push(element as T));
+        return resources;
+    }
 
-  protected jsonDataToResource(jsonData: any): T{
-    return jsonData as T;
-  }
+    protected jsonDataToResource(jsonData: any): T{
+        return jsonData as T;
+    }
 
-  protected handleError(error: any): Observable<any>{
-    console.log("Error na Requisição =>", error);
-    return throwError(error);
-  }
-
+    protected handleError(error: any): Observable<any>{
+        console.log("Error na Requisição =>", error);
+        return throwError(error);
+    }
 }
