0e1a99a4b989a06c348203e2cf7e7d74008750ef
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-02-28 22:41:12 -0300

    adicionando routerModule ao módulo SharedModule e deixando dinamicamente os componentes da página com BreadCrumbComponent

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -4,6 +4,7 @@ import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { EntriesRoutingModule } from './entries-routing.module';
 
+import { BreadCrumbComponent } from 'src/app/shared/components/bread-crumb/bread-crumb.component';
 import { CalendarModule } from 'primeng/calendar';
 import { IMaskModule } from 'angular-imask';
 import { SharedModule } from 'src/app/shared/shared.module';
