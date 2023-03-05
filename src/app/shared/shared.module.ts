4cdef54ee3ac1b22f563e52b390ad455e79730ca
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-03-04 22:30:15 -0300

    adicionando component FormFildError

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent, PageHeaderComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent, FormFieldErrorComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule
   ],
-  exports: [ CommonModule, ReactiveFormsModule, BreadCrumbComponent, PageHeaderComponent ]
+  exports: [ CommonModule, ReactiveFormsModule, BreadCrumbComponent, PageHeaderComponent, FormFieldErrorComponent ]
 })
 export class SharedModule { }
